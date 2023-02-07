3435371c52f576a904f9fcd6ee2a0288087dcaff
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-02-07 11:23:03 +0100

    search by name

--- searchByName.js
+++ searchByName.js
@@ -1,9 +1,11 @@
 const getBeers = require('./data')
 
-
 function main(keyword) {
     console.log('keyword:', keyword)
     // Tip: string.prototype.includes()
+    const beers = getBeers();
+    const searchResults = beers.filter((beer) => beer.name.includes(keyword))
+    searchResults.forEach((beer) => console.log(beer.name))
 }
 
 main(process.argv[2])
